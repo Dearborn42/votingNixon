98f01fd06d95b5d6cb51886b0dc27939652226b2
AndrewMurphy <amurf26@outlook.com>
2023-01-26 18:22:24 -0700

    creating swapping elements

--- script.js
+++ script.js
@@ -1,73 +1,25 @@
+class Nixon {
+    constructor() {
+        this.div1 = document.createElement("div");
+        this.div1.style.width = "100px";
+        this.div1.style.height = "100px";
+        this.div1.style.backgroundColor = "red";
+        this.div1.addEventListener("mouseover", this.swapDivs.bind(this));
+        document.body.appendChild(this.div1);
 
-
-// for(let i=0; i<2; i++){
-    
-//     if(i==0)
-        
-//     else
-//         bigDiv.style.backgroundColor = "blue";
-    
-//     let button = document.createElement("button");
-//     if(i == 0)
-//         button.innerHTML = "Vote for Nixon";
-//     else
-//         butt
-//     for(let j=0; j<1; j++){
-//         let pic = document.createElement("img");
-//         if(i==0)
-//             pic.src = 'Nixon.jpg';
-//         else    
-//             pic.src = 'George.jpg';
-//         pic.style.height = "30vh";
-//         pic.style.width = "20vw";
-//         bigDiv.append(pic);
-//     }
-//     page.append(bigDiv);
-// }
-
-// document.body.style.margin = "0";
-
-
-
-
-
-
-
-
-
-class Nixon{
-    page(){
-        this.page = document.createElement("div");
-        this.page.style.width = "100vw";
-        this.page.style.height = "100vh";
-        this.page.style.display = "flex";
-        this.page.style.justifyContent = "space-evenly";
-        this.page.style.margin = "0";
-        this.rightSide();
-        this.wrongSide();
-        document.body.appendChild(this.page);
-        return this;
+        this.div2 = document.createElement("div");
+        this.div2.style.width = "100px";
+        this.div2.style.height = "100px";
+        this.div2.style.backgroundColor = "blue";
+        this.div2.addEventListener("mouseover", this.swapDivs.bind(this));
+        document.body.appendChild(this.div2);
     }
-    rightSide(){
-        this.bigDiv1 = document.createElement("div");
-        this.bigDiv1.style.width = "100vw";
-        this.bigDiv1.style.height = "100vh";
-        this.bigDiv1.style.display = "flex";
-        this.bigDiv1.style.justifyContent = "center";
-        this.bigDiv1.style.backgroundColor = "red";
-        this.page.append(this.bigDiv1);
-    }
-    wrongSide(){
-        this.bigDiv2 = document.createElement("div");
-        this.bigDiv2.style.width = "100vw";
-        this.bigDiv2.style.height = "100vh";
-        this.bigDiv2.style.display = "flex";
-        this.bigDiv2.style.justifyContent = "center";
-        this.bigDiv2.style.backgroundColor = "blue";
-        this.page.append(this.bigDiv2);
+
+    swapDivs() {
+        let div1Background = this.div1.style.backgroundColor;
+        this.div1.style.backgroundColor = this.div2.style.backgroundColor;
+        this.div2.style.backgroundColor = div1Background;
     }
 }
 
-let vote = new Nixon();
-
-vote.page();
+let nixon = new Nixon();
